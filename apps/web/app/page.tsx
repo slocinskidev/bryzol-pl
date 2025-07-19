6d4998e13700963162806dda4da6d86b067c4ded
Eryk Słociński <eryk.slocinski@gmail.com>
2025-07-19 09:57:30 +0200

    Initial commit

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,9 @@
+import HeroSectionOne from "@/components/hero";
+
+export default function Page() {
+	return (
+		<div className="bg-white dark:bg-black transition-colors duration-500">
+			<HeroSectionOne />
+		</div>
+	);
+}
